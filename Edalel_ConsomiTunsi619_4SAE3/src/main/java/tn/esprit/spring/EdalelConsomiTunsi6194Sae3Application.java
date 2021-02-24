459d9248ef52da95200faba70362686914b2892a
Aziz_Mathlouthi <mohamedaziz.mathlouthi@esprit.tn>
2021-02-25 00:05:43 +0100

    first commit

---
+++ Edalel_ConsomiTunsi619_4SAE3/src/main/java/tn/esprit/spring/EdalelConsomiTunsi6194Sae3Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdalelConsomiTunsi6194Sae3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdalelConsomiTunsi6194Sae3Application.class, args);
+	}
+
+}
